e02bcaf955d326f266900e2522505b4706aa3d8f
Keith Williams <71899353+k-will22@users.noreply.github.com>
2021-04-24 02:23:37 -0400

    Time: 80 ms (74.14%), Space: 39.7 MB (89.81%) - LeetHub

---
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function(grid) {
+    let total = 0
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] < 0) {
+                total++
+            }
+        }
+    }
+    return total
+};
