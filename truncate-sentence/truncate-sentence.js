a1c062a18367438ddeb6b423dd50174c9f0e8fb3
Keith Williams <71899353+k-will22@users.noreply.github.com>
2021-05-13 01:55:40 -0400

    Time: 80 ms (54.45%), Space: 38.8 MB (22.69%) - LeetHub

---
+++ truncate-sentence/truncate-sentence.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    let arr = s.split(" ")
+    return arr.slice(0, k).join(" ")
+    
+};
