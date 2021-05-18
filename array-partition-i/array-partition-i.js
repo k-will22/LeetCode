2edeb1b1e29e08ba6c7831cb4174a68bea602e6f
Keith Williams <71899353+k-will22@users.noreply.github.com>
2021-05-17 21:13:10 -0400

    Time: 136 ms (38.33%), Space: 44.1 MB (62.32%) - LeetHub

---
+++ array-partition-i/array-partition-i.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    let total = 0
+    nums.sort((a, b) => a-b)
+    
+    for (let i = 0; i < nums.length; i += 2) {
+        total += nums[i]
+    }
+    return total
+};
